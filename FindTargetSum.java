36bc28b9303be7f2ac42b1f64ba42da4ddbbb0a3
GitH05 <santoshdhawal05@gmail.com>
2026-09-11 11:30:37 +0545

    Ref:

--- FindTargetSum.java
+++ FindTargetSum.java
@@ -1,10 +1,10 @@
 import java.util.HashMap;
 
-public class TwoSum {
+public class FindTargetSum {
 
     public static void main(String[] args) {
 
-        int[] nums = {2, 7, 11, 15, 1, 8, 3, 6};
+        int[] nums = { 2, 7, 11, 15, 1, 8, 3, 6 };
         int target = 9;
 
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -12,8 +12,11 @@ public class TwoSum {
         for (int i = 0; i < nums.length; i++) {
 
             int need = target - nums[i]; // Number needed to reach target
+            // [when i = 0, need = 9 - 2, 7 needed | when i = 1, need 9 - 7, 2 needed]
 
             if (map.containsKey(need)) { // Check if we already have that number
+                // [when i = 0, need = 7, map not have 7 | when i = 1, need = 2 map have 2 =>
+                // CONDITION TRUE]
 
                 int firstIndex = map.get(need);
                 int secondIndex = i;
@@ -28,6 +31,7 @@ public class TwoSum {
             }
 
             map.put(nums[i], i); // Store number and its index
+            // [when i = 0, store (2, 0) and when i = 1, store (7, 1) ]
         }
 
         System.out.println("No pair found");
